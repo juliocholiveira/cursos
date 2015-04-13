ad5f3d6d6bf508de82aa4d4dfa28c91d92d591fa
Júlio Oliveira <juliocholiveira@gmail.com>
2015-04-13 20:19:00 -0300

    ajustes

--- curso-alura-jpa/src/test/java/br/com/caelum/financas/teste/TesteJPA.java
+++ curso-alura-jpa/src/test/java/br/com/caelum/financas/teste/TesteJPA.java
@@ -5,25 +5,34 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
 
 public class TesteJPA {
 
 	public static void main(String[] args) {
 		
+		Long inicio = System.currentTimeMillis();
+		
 		Conta conta = new Conta();
 		
 		conta.setAgencia("3853");
-		conta.setBanco("BB");
+		conta.setBanco("Júlio César");
 		conta.setNumero("123456");
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas-mysql");
-		EntityManager em = emf.createEntityManager();
+		//EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas-mysql");
+		//EntityManager em = emf.createEntityManager();
+		
+		EntityManager em = new JPAUtil().getEntityManager();
 		
 		em.getTransaction().begin();
 		em.persist(conta);
 		em.getTransaction().commit();
 		
 		em.close();
+		
+		Long fim = System.currentTimeMillis();
+		
+		System.out.println("Demorou: " + ((fim-inicio)/1000) + " s");
 	}
-
 }
