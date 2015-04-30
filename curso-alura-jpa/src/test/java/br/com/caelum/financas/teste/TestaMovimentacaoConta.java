c1c2986110e9b5890ddcf3218b191a9e0799aefd
f6015097 <f6015097@ESNTL018503P302.intrabb.bb.com.br>
2015-04-30 18:03:12 -0300

    ok

--- curso-alura-jpa/src/test/java/br/com/caelum/financas/teste/TestaMovimentacaoConta.java
+++ curso-alura-jpa/src/test/java/br/com/caelum/financas/teste/TestaMovimentacaoConta.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
 import br.com.caelum.financas.util.JPAUtil;
 
 public class TestaMovimentacaoConta {
@@ -18,13 +19,28 @@ public class TestaMovimentacaoConta {
 		
 		System.out.println("Total de movimentações: " + conta.getMovimentacoes().size());*/
 		
-		Query query = em.createQuery("from Conta c join fetch c.movimentacoes");
+		/*Query query = em.createQuery("from Conta c join fetch c.movimentacoes");
 		
 		List<Conta> contas = query.getResultList();
 		
 		for (Conta conta2 : contas) {
 			System.out.println("Total de movimentações: " + conta2.getMovimentacoes().size());
+		}*/
+		
+		Conta c = new Conta();
+		c = em.find(Conta.class, 2);
+		
+		Query query = em.createQuery("select m from Movimentacao m where m.conta = :pConta");
+		query.setParameter("pConta", c);
+		
+		List<Movimentacao> lista = query.getResultList();
+		
+		for (Movimentacao movimentacao : lista) {
+			System.out.println(movimentacao.getDescricao());
 		}
+		
+		System.out.println("Fim");
+				
 		
 	}
 
