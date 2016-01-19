d782a5148791aad9126226695c1b23c2a05d85f9
F6015097 <F6015097@ESNTL018503P302.intrabb.bb.com.br>
2016-01-19 14:43:02 -0300

    Ajustes

---
+++ curso-alura-produtividade-eclipse/src/br/com/caelum/model/TesteDebug.java
@@ -0,0 +1,16 @@
+package br.com.caelum.model;
+
+public class TesteDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Passo");
+		
+		String s = "Júlio";
+		
+		System.out.println(s);
+
+	}
+
+}
---
+++ curso-alura-produtividade-eclipse/src/br/com/caelum/test/TesteJUnit.java
@@ -0,0 +1,16 @@
+package br.com.caelum.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteJUnit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		assertTrue("", 0 == 1);
+	}
+
+}
